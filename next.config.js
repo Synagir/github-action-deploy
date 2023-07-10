48940f10a27f448b8665fc5fa39ab6072473d197
jin-young-lee <jin-young-lee@nts-corp.com>
2023-07-10 18:06:39 +0900

    update next.config.js, package.json

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: '/github-action-deploy'
+  basePath: '/github-action-deploy',
+  output: 'export',
 }
 
 module.exports = nextConfig
