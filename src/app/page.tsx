9b353c650a0ffbc936b04b8ce3467b1df7f5fc58
jin-young-lee <jin-young-lee@nts-corp.com>
2023-07-11 17:24:39 +0900

    update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 import NextLogo from '@img/next.svg'
-import VercelLogo from '@img/next.svg'
+import VercelLogo from '@img/vercel.svg'
 import styles from './page.module.css'
 
 export default function Home() {
